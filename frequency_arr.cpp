5ced6885945deab390432519e89fca006a5f43ca
muhaiminverse <muhaiminverse@gmail.com>
2023-10-21 09:05:47 +0600

    daily update

---
+++ frequency_arr.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void freq2(vector<int>& nums){
+ 
+    map<int,int>mp;
+ 
+    for (int i = 0; i < nums.size(); i++)
+    {
+        mp[nums[i]]++;
+    }
+    
+     
+    
+}
+ 
+int main()
+{
+    int n,f;
+    cin>>n>>f;
+    int m;
+    vector<int>a;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>m;
+        a.push_back(m);
+    }
+    
+    freq2(a);
+ 
+}
